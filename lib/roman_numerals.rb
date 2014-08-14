6d0d30886e1c9dbdcdd952a19d5a3916c074d5d5
ETHEL NG <ethel.ng@gmail.com>
2014-08-15 00:36:02 +0100

    Minor refactoring to remove unnecessary 'return'

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -3,19 +3,19 @@ NUMERALS = { 1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1
 def romanize_ones(ones)
 	if ones > 0 && ones <= 5
 		if ones % 5 == 0
-			return NUMERALS[5]
+			NUMERALS[5]
 		elsif ones % 5 == 4
-			return "#{NUMERALS[1]}#{NUMERALS[5]}"
+			"#{NUMERALS[1]}#{NUMERALS[5]}"
 		else
-			return "#{NUMERALS[1] * ones}"
+			"#{NUMERALS[1] * ones}"
 		end
 	elsif ones > 5
 		if ones % 5 == 0
-			return NUMERALS[10]
+			NUMERALS[10]
 		elsif ones % 5 == 4
-			return "#{NUMERALS[1]}#{NUMERALS[10]}"
+			"#{NUMERALS[1]}#{NUMERALS[10]}"
 		else
-			return "#{NUMERALS[5]}#{NUMERALS[1] * (ones - 5)}"
+			"#{NUMERALS[5]}#{NUMERALS[1] * (ones - 5)}"
 		end
 	end
 end
@@ -23,19 +23,19 @@ end
 def romanize_tens(tens)
 	if tens > 0 && tens <= 5
 		if tens % 5 == 0
-			return NUMERALS[50]
+			NUMERALS[50]
 		elsif tens % 5 == 4
-			return "#{NUMERALS[10]}#{NUMERALS[50]}"
+			"#{NUMERALS[10]}#{NUMERALS[50]}"
 		else
-			return "#{NUMERALS[10] * tens}"
+			"#{NUMERALS[10] * tens}"
 		end
 	elsif tens > 5
 		if tens % 5 == 0
-			return NUMERALS[100]
+			NUMERALS[100]
 		elsif tens % 5 == 4
-			return "#{NUMERALS[10]}#{NUMERALS[100]}"
+			"#{NUMERALS[10]}#{NUMERALS[100]}"
 		else
-			return "#{NUMERALS[50]}#{NUMERALS[10] * (tens - 5)}"
+			"#{NUMERALS[50]}#{NUMERALS[10] * (tens - 5)}"
 		end
 	end
 end
@@ -43,19 +43,19 @@ end
 def romanize_hundreds(hundreds)
 	if hundreds > 0 && hundreds <= 5
 		if hundreds % 5 == 0
-			return NUMERALS[500]
+			NUMERALS[500]
 		elsif hundreds % 5 == 4
-			return "#{NUMERALS[100]}#{NUMERALS[500]}"
+			"#{NUMERALS[100]}#{NUMERALS[500]}"
 		else
-			return "#{NUMERALS[100] * hundreds}"
+			"#{NUMERALS[100] * hundreds}"
 		end
 	elsif hundreds > 5
 		if hundreds % 5 == 0
-			return NUMERALS[1000]
+			NUMERALS[1000]
 		elsif hundreds % 5 == 4
-			return "#{NUMERALS[100]}#{NUMERALS[1000]}"
+			"#{NUMERALS[100]}#{NUMERALS[1000]}"
 		else
-			return "#{NUMERALS[500]}#{NUMERALS[100] * (hundreds - 5)}"
+			"#{NUMERALS[500]}#{NUMERALS[100] * (hundreds - 5)}"
 		end
 	end
 end
@@ -63,19 +63,19 @@ end
 def romanize_thousands(thousands)
 	if thousands > 0 && thousands <= 5
 		if thousands % 5 == 0
-			return NUMERALS[5000]
+			NUMERALS[5000]
 		elsif thousands % 5 == 4
-			return "#{NUMERALS[1000]}#{NUMERALS[5000]}"
+			"#{NUMERALS[1000]}#{NUMERALS[5000]}"
 		else
-			return "#{NUMERALS[1000] * thousands}"
+			"#{NUMERALS[1000] * thousands}"
 		end
 	elsif thousands > 5
 		if thousands % 5 == 0
-			return NUMERALS[10000]
+			NUMERALS[10000]
 		elsif thousands % 5 == 4
-			return "#{NUMERALS[1000]}#{NUMERALS[10000]}"
+			"#{NUMERALS[1000]}#{NUMERALS[10000]}"
 		else
-			return "#{NUMERALS[5000]}#{NUMERALS[1000] * (hundreds - 5)}"
+			"#{NUMERALS[5000]}#{NUMERALS[1000] * (hundreds - 5)}"
 		end
 	end
 end
@@ -86,5 +86,5 @@ def romanize(number)
 	hundreds = number.to_s[-3].to_i
 	thousands = number.to_s[-4].to_i
 
-	return "#{romanize_thousands(thousands)}#{romanize_hundreds(hundreds)}#{romanize_tens(tens)}#{romanize_ones(ones)}"
+	"#{romanize_thousands(thousands)}#{romanize_hundreds(hundreds)}#{romanize_tens(tens)}#{romanize_ones(ones)}"
 end
